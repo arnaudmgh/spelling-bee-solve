d615ef2c49b0bcc2654de279b259cf4dc0a998d7
Arnaud Amzallag <arnaud.amzallag@gmail.com>
2020-11-15 01:04:59 -0500

    replacing regex-tdfa by pcre

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,15 +3,15 @@ module Lib
     ( isConform
     ) where
 
-import Text.Regex.TDFA
-import Text.Regex.TDFA.Text ()
+import Text.Regex.PCRE
+-- import Text.Regex.TDFA.Text ()
 
 isConform :: IO ()
 isConform = do 
     -- line <- getLine
     line <- getContents
     -- let emailRegex = "[a-zA-Z0-9+._-]+@[a-zA-Z-]+\\.[a-z]+"
-    let gameRegex = "[cioytnu]*c[cioytnu]*"
+    let gameRegex = "^[cioytnu]*c[cioytnu]*\\EOL"
     let output = getAllTextMatches $ line =~ gameRegex :: [String]
     putStrLn $ unlines $ filter at_least_4_letters output where
         at_least_4_letters = (>4) . length
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Lib
+
+import Lib (isConform)
+-- main :: IO ()
+main :: [Char] -> Bool
+-- main = putStrLn "Test suite not yet implemented"
+main = isConform "tycoon glah, cy, " == "tycoon\n"
