873c573e4300f158b67bca7d7787648edf9b1228
Arnaud Amzallag <arnaud.amzallag@gmail.com>
2020-11-16 21:15:54 -0500

    removing 'where' keyword... seems to still work...

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,7 +13,7 @@ isConform gameRegex = do
     line <- getContents
     -- let gameRegex = "\\<[cioytnu]*c[cioytnu]*\\>"
     let output = getAllTextMatches $ line =~ gameRegex :: [String]
-    let output_final = filter at_least_4_letters output where
+    let output_final = filter at_least_4_letters output
         at_least_4_letters = (>=4) . length
     putStrLn $ unlines output_final
     putStrLn $ show (length output_final) ++ " words total"
